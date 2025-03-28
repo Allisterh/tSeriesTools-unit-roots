9ad4817e63f44818cda9ad26b3d82f8acd79cd27
Marina FERENT <marinaferent@gmail.com>
2025-03-28 17:21:14 +0200

    Update forecastOriginal_quarterly.r
    Updated to include computation of prediction intervals

--- R/forecastOriginal_quarterly.r
+++ R/forecastOriginal_quarterly.r
@@ -25,7 +25,7 @@
 #' arimaEstimation=arima(Tbill3M_stationary, order = c(1,0,0))
 #' forecastOriginal_quarterly(arimaEstimation, type, seasonalCoeff, seasonalityType="multiplicative", nQuarters=8, year=1991, quarter=1, saSeries=Tbill3M_sa)
 
-forecastOriginal_quarterly=function(arimaEstimation, type, seasonalCoeff, seasonalityType="none", nQuarters=4, year, quarter, saSeries)
+forecastOriginal_quarterly=function(arimaEstimation, type, seasonalCoeff, seasonalityType="none", nQuarters=4, year, quarter, saSeries, predictionIntervals=c(80,90))
 {
   modelOrder=eval(arimaEstimation$call$order)
   if(modelOrder[2]==0)
@@ -36,98 +36,101 @@ forecastOriginal_quarterly=function(arimaEstimation, type, seasonalCoeff, season
   }
 
   #Step 1: Forecast the stationary series
-	forecast=forecast(arimaEstimation, h=nQuarters) #the forecasted values with prediction intervals
+	forecast=forecast(arimaEstimation, h=nQuarters, level=predictionIntervals) #the forecasted values with prediction intervals
 	forecast_x_sa_stationary=as.data.frame(forecast)
-	forecast_x_sa_stationary_ts=ts(forecast_x_sa_stationary[,1], start=c(year,quarter), frequency=4)
-	
-	#Step 2: Forecast the original series
-	if(type=="Stationary, no trend")
+	for(series in 1:(length(predictionIntervals)*2+1))
 	{
-		#in this case, the seasonally adjusted forecasted series is the same as the seasonally adjusted stationary series:
-		forecast_x_sa=forecast_x_sa_stationary_ts
-	} else {
-		if(type=="Stationary around a linear trend")
+		forecast_x_sa_stationary_ts=ts(forecast_x_sa_stationary[,series], start=c(year,quarter), frequency=4)
+		#Step 2: Forecast the original series
+		if(type=="Stationary, no trend")
 		{
-			trend=1:length(x)
-			regression=lm(x~trend, data.frame(trend, x))
-			x_stationary=regression$residuals
-			#x_stationary=ts(x_stationary, start=c(yearStartSeries, quarterStartSeries), frequency=4)
-			intercept=summary(regression)$coeff[1,1]
-			betaTrend=summary(regression)$coeff[2,1]
-
-			trendF=(arimaEstimation$nobs+1):(arimaEstimation$nobs+nQuarters)
-			forecast_x_sa=intercept + betaTrend*trendF + forecast_x_sa_stationary_ts
-			forecast_x_sa=ts(forecast_x_sa, start=c(year,quarter), frequency=4)
+			#in this case, the seasonally adjusted forecasted series is the same as the seasonally adjusted stationary series:
+			forecast_x_sa=forecast_x_sa_stationary_ts
 		} else {
-			if(type=="Random walk with drift")
+			if(type=="Stationary around a linear trend")
 			{
-				###since the first difference is the stationary series, we compute the original sa series by adding the forecasted difference to the
-				####last element of the series, and so on....
-				yearStartSeries=year-as.integer(length(saSeries)/4)
-				q=(length(saSeries)/4-as.integer(length(saSeries)/4))/0.25
-				quarterStartSeries=quarter-q
-				x_sa_ts=ts(saSeries, start=c(yearStartSeries,quarterStartSeries), frequency=4)
-				forecast_x_sa=c()
-				forecast_x_sa[1]=x_sa_ts[length(x_sa_ts)] + forecast_x_sa_stationary_ts[1]
-				for (i in 2:length(forecast_x_sa_stationary_ts))
-				{
-					forecast_x_sa[i]=forecast_x_sa[i-1] + forecast_x_sa_stationary_ts[i]
-				}
+				trend=1:length(x)
+				regression=lm(x~trend, data.frame(trend, x))
+				x_stationary=regression$residuals
+				#x_stationary=ts(x_stationary, start=c(yearStartSeries, quarterStartSeries), frequency=4)
+				intercept=summary(regression)$coeff[1,1]
+				betaTrend=summary(regression)$coeff[2,1]
+
+				trendF=(arimaEstimation$nobs+1):(arimaEstimation$nobs+nQuarters)
+				forecast_x_sa=intercept + betaTrend*trendF + forecast_x_sa_stationary_ts
 				forecast_x_sa=ts(forecast_x_sa, start=c(year,quarter), frequency=4)
 			} else {
-				if(type=="Unit root and linear trend")
+				if(type=="Random walk with drift")
 				{
-					#to compute the forecasted series:
-					#diff(x_sa)_forecast=intercept + betaTrend*trendF + forecast_x_sa_stationary_ts
-					##1. we compute the forecasted first difference:
-					###computing the intercept and betaTrend:
-					trend=1:length(diff(saSeries))
-					regression=lm(diff(saSeries)~trend, data.frame(trend, diff(saSeries)))
-					intercept=summary(regression)$coeff[1,1]
-					betaTrend=summary(regression)$coeff[2,1]
-
-					### computing the forecasted first difference:
-					trendF=(arimaEstimation$nobs+1):(arimaEstimation$nobs+nQuarters)
-					forecast_diff_x_sa=intercept + betaTrend*trendF + forecast_x_sa_stationary_ts
-					forecast_diff_x_sa_ts=ts(forecast_diff_x_sa, start=c(year,quarter), frequency=4)
-
-					##2.we compute the forecasted_sa series:
+					###since the first difference is the stationary series, we compute the original sa series by adding the forecasted difference to the
+					####last element of the series, and so on....
 					yearStartSeries=year-as.integer(length(saSeries)/4)
 					q=(length(saSeries)/4-as.integer(length(saSeries)/4))/0.25
 					quarterStartSeries=quarter-q
 					x_sa_ts=ts(saSeries, start=c(yearStartSeries,quarterStartSeries), frequency=4)
 					forecast_x_sa=c()
-					forecast_x_sa[1]=x_sa_ts[length(x_sa_ts)] + forecast_diff_x_sa_ts[1]
-					for (i in 2:length(forecast_diff_x_sa_ts))
+					forecast_x_sa[1]=x_sa_ts[length(x_sa_ts)] + forecast_x_sa_stationary_ts[1]
+					for (i in 2:length(forecast_x_sa_stationary_ts))
 					{
-						forecast_x_sa[i]=forecast_x_sa[i-1] + forecast_diff_x_sa_ts[i]
+						forecast_x_sa[i]=forecast_x_sa[i-1] + forecast_x_sa_stationary_ts[i]
 					}
 					forecast_x_sa=ts(forecast_x_sa, start=c(year,quarter), frequency=4)
+				} else {
+					if(type=="Unit root and linear trend")
+					{
+						#to compute the forecasted series:
+						#diff(x_sa)_forecast=intercept + betaTrend*trendF + forecast_x_sa_stationary_ts
+						##1. we compute the forecasted first difference:
+						###computing the intercept and betaTrend:
+						trend=1:length(diff(saSeries))
+						regression=lm(diff(saSeries)~trend, data.frame(trend, diff(saSeries)))
+						intercept=summary(regression)$coeff[1,1]
+						betaTrend=summary(regression)$coeff[2,1]
+
+						### computing the forecasted first difference:
+						trendF=(arimaEstimation$nobs+1):(arimaEstimation$nobs+nQuarters)
+						forecast_diff_x_sa=intercept + betaTrend*trendF + forecast_x_sa_stationary_ts
+						forecast_diff_x_sa_ts=ts(forecast_diff_x_sa, start=c(year,quarter), frequency=4)
+
+						##2.we compute the forecasted_sa series:
+						yearStartSeries=year-as.integer(length(saSeries)/4)
+						q=(length(saSeries)/4-as.integer(length(saSeries)/4))/0.25
+						quarterStartSeries=quarter-q
+						x_sa_ts=ts(saSeries, start=c(yearStartSeries,quarterStartSeries), frequency=4)
+						forecast_x_sa=c()
+						forecast_x_sa[1]=x_sa_ts[length(x_sa_ts)] + forecast_diff_x_sa_ts[1]
+						for (i in 2:length(forecast_diff_x_sa_ts))
+						{
+							forecast_x_sa[i]=forecast_x_sa[i-1] + forecast_diff_x_sa_ts[i]
+						}
+						forecast_x_sa=ts(forecast_x_sa, start=c(year,quarter), frequency=4)
+					}
+				}
+			}
+		}
+		#Step 3: Add seasonality to the original forecasted series
+		if(seasonalityType=="additive")
+		{
+			seasonalCoeff_ts=ts(rep(seasonalCoeff, as.integer(nQuarters/4 + 1)), start=c(year,1), frequency=4)
+			forecast_x=forecast_x_sa + seasonalCoeff_ts
+			forecast_x=as.data.frame(forecast_x)
+		} else {
+			if(seasonalityType=="multiplicative")
+			{
+				seasonalCoeff_ts=ts(rep(seasonalCoeff, as.integer(nQuarters/4 + 1)), start=c(year,1), frequency=4)
+				forecast_x=forecast_x_sa*seasonalCoeff_ts
+				forecast_x=as.data.frame(forecast_x)
+			} else {
+				if(seasonalityType=="none")
+				{
+					forecast_x=forecast_x_sa
+					forecast_x=as.data.frame(forecast_x)
 				}
 			}
 		}
 	}
-	#Step 3: Add seasonality to the original forecasted series
-	if(seasonalityType=="additive")
-	{
-		seasonalCoeff_ts=ts(rep(seasonalCoeff, as.integer(nQuarters/4 + 1)), start=c(year,1), frequency=4)
-		forecast_x=forecast_x_sa + seasonalCoeff_ts
-		forecast_x=as.data.frame(forecast_x)
-	} else {
-		if(seasonalityType=="multiplicative")
-		{
-			seasonalCoeff_ts=ts(rep(seasonalCoeff, as.integer(nQuarters/4 + 1)), start=c(year,1), frequency=4)
-			forecast_x=forecast_x_sa*seasonalCoeff_ts
-			forecast_x=as.data.frame(forecast_x)
-		} else {
-			if(seasonalityType=="none")
-			{
-				forecast_x=forecast_x_sa
-				forecast_x=as.data.frame(forecast_x)
-			}
-		}
-	}
 	rownames(forecast_x)=rownames(forecast_x_sa_stationary)
-	colnames(forecast_x)=model
+	colnames(forecast_x)[1]=model
+	colnames(forecast_x)[2:ncol(forecast_x)]=colnames(forecast_x_sa_stationary)[2:ncol(forecast_x_sa_stationary)]
 	forecast_x
 }
